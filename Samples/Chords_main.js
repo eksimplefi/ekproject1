1c9996591dcf73a0b5d18e5940ea5fb51f0ff317
eksimplefi <ek.simplefi@icloud.com>
2026-03-09 13:52:36 -0400

    Update Chords_main.js

--- Samples/Chords_main.js
+++ Samples/Chords_main.js
@@ -9,23 +9,8 @@ class ChordDependencyWidget extends HTMLElement {
     const container = document.createElement("div");
     container.id = "chord-container";
     container.style.cssText = "width:100%;height:100%;";
+    container.textContent = "Minimal widget, no D3.";
     this.shadowRoot.appendChild(container);
-
-    const script = document.createElement("script");
-    script.src = "https://d3js.org/d3.v7.min.js";
-    script.onload = () => {
-      this._onD3Loaded(container);
-    };
-    script.onerror = (e) => {
-      container.textContent = "Failed to load D3.";
-      console.error("Failed to load D3", e);
-    };
-    this.shadowRoot.appendChild(script);
-  }
-
-  _onD3Loaded(container) {
-    container.textContent = "";
-
   }
 
   onCustomWidgetAfterUpdate(changedProps) {}
